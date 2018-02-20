c9d42e3387583655d89f7bd64ddb3bc1945948cf
Artem <chilly.artem@gmail.com>
2018-02-20 14:51:58 +0300

    test

---
+++ src/com/epam/artem_berdnik/java/lesson1/task1/hello_world.java
@@ -0,0 +1,10 @@
+package com.epam.artem_berdnik.java.lesson1.task1;
+
+/**
+ * Created by Artem_Berdnik on 2/20/2018.
+ */
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
